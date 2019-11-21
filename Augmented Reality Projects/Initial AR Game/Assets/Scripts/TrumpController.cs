81f37a7a5cfb0f2703af9c2774aae0c71cb16625
Anmol Kumar <azvenuskestel19@outlook.com>
2019-11-21 22:05:00 +0530

    .

---
+++ Augmented Reality Projects/Initial AR Game/Assets/Scripts/TrumpController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrumpController : MonoBehaviour
+{
+    private Rigidbody rigidB;
+    private Animation animT;
+    public GameObject mobileB;
+    public float velocityF = 4f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidB = GetComponent<Rigidbody>();
+        animT = GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = (mobileB.GetComponent<RectTransform>().position.x) - 152f;
+        float y = (mobileB.GetComponent<RectTransform>().position.y) - 119f;
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rigidB.velocity = movement / velocityF;
+
+        if (x!=0 && y!=0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x,y)*Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+
+        if (x!=0 || y!=0)
+        {
+            animT.Play("Walk");
+        }
+        else
+        {
+            animT.Play("Idle");
+        }
+        //Debug.Log(x);
+        //Debug.Log(y);
+    }
+}
